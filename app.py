c2b3866ba1431a1f6f0cbdc447653a87f2219cd6
Cary Kinsfather <cary.kinsfather@gmail.com>
2023-05-19 11:18:43 -0500

    working

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from flask import Flask, redirect, render_template, request, url_for
 app = Flask(__name__)
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-
+@app.route("/home", methods=("GET", "POST"))
 @app.route("/", methods=("GET", "POST"))
 def index():
     if request.method == "POST":
@@ -14,22 +14,33 @@ def index():
         response = openai.Completion.create(
             model="text-davinci-003",
             prompt=generate_prompt(animal),
-            temperature=0.6,
+            max_tokens=1000,
+            temperature=0,
+            top_p=1,
+            n=1,
+            # decription below
+            # n = 1 means that we only want one response
         )
+        print(response)
         return redirect(url_for("index", result=response.choices[0].text))
+
 
     result = request.args.get("result")
     return render_template("index.html", result=result)
 
 
 def generate_prompt(animal):
-    return """Suggest three names for an animal that is a superhero.
+    return """Create an HTML table of the top 5 {}.""".format(animal.capitalize())
 
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: {}
-Names:""".format(
-        animal.capitalize()
-    )
+
+'''
+"""Return the number of people that live in the following city as a number
+    City: Houston, Texas
+    Value: 2,356,000
+    City: Austin
+
+    Value: 1,356,000
+    City: San Antonio
+    Value:
+    """
+'''
